c95eae3be5612c899d75b377c698f9aa20fd52fd
Miguel Grinberg <miguel.grinberg@gmail.com>
2022-01-25 18:15:39 +0000

    Chapter 5: Connecting to a Back End (v0.5)

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,37 +1,43 @@
+import { useState, useEffect } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+import Post from './Post';
+
+const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
+
 export default function Posts() {
-  const posts = [
-    {
-      id: 1,
-      text: 'Hello, world!',
-      timestamp: 'a minute ago',
-      author: {
-        username: 'susan',
-      },
-    },
-    {
-      id: 2,
-      text: 'Second post',
-      timestamp: 'an hour ago',
-      author: {
-        username: 'john',
-      },
-    },
-  ];
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(BASE_API_URL + '/api/feed');
+      if (response.ok) {
+        const results = await response.json();
+        setPosts(results.data);
+      }
+      else {
+        setPosts(null);
+      }
+    })();
+  }, []);
 
   return (
     <>
-      {posts.length === 0 ?
-        <p>There are no blog posts.</p>
+      {posts === undefined ?
+        <Spinner animation="border" />
       :
-        posts.map(post => {
-          return (
-            <p key={post.id}>
-              <b>{post.author.username}</b> &mdash; {post.timestamp}
-              <br />
-              {post.text}
-            </p>
-          );
-        })
+        <>
+          {posts === null ?
+            <p>Could not retrieve blog posts.</p>
+          :
+            <>
+              {posts.length === 0 ?
+                <p>There are no blog posts.</p>
+              :
+                posts.map(post => <Post key={post.id} post={post} />)
+              }
+            </>
+          }
+        </>
       }
     </>
   );
