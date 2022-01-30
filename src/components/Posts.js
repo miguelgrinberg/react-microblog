db3d54316b7c593e657c6b219cd4ebb00e8c35f2
Miguel Grinberg <miguel.grinberg@gmail.com>
2022-01-30 00:35:31 +0000

    Chapter 6: Building an API Client (v0.6)

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,24 +1,50 @@
 import { useState, useEffect } from 'react';
 import Spinner from 'react-bootstrap/Spinner';
+import { useApi } from '../contexts/ApiProvider';
 import Post from './Post';
+import More from './More';
 
-const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
-
-export default function Posts() {
+export default function Posts({ content }) {
   const [posts, setPosts] = useState();
+  const [pagination, setPagination] = useState();
+  const api = useApi();
+
+  let url;
+  switch (content) {
+    case 'feed':
+    case undefined:
+      url = '/feed';
+      break;
+    case 'explore':
+      url = '/posts';
+      break
+    default:
+      url = `/users/${content}/posts`;
+      break;
+  }
 
   useEffect(() => {
     (async () => {
-      const response = await fetch(BASE_API_URL + '/api/feed');
+      const response = await api.get(url);
       if (response.ok) {
-        const results = await response.json();
-        setPosts(results.data);
+        setPosts(response.body.data);
+        setPagination(response.body.pagination);
       }
       else {
         setPosts(null);
       }
     })();
-  }, []);
+  }, [api, url]);
+
+  const loadNextPage = async () => {
+    const response = await api.get(url, {
+      after: posts[posts.length - 1].timestamp
+    });
+    if (response.ok) {
+      setPosts([...posts, ...response.body.data]);
+      setPagination(response.body.pagination);
+    }
+  };
 
   return (
     <>
@@ -39,6 +65,7 @@ export default function Posts() {
           }
         </>
       }
+      <More pagination={pagination} loadNextPage={loadNextPage} />
     </>
   );
 }
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,13 +1,52 @@
+import { useState, useEffect } from 'react';
+import Stack from 'react-bootstrap/Stack';
+import Image from 'react-bootstrap/Image';
+import Spinner from 'react-bootstrap/Spinner';
 import { useParams } from 'react-router-dom';
 import Body from '../components/Body';
+import Posts from '../components/Posts';
+import TimeAgo from '../components/TimeAgo';
+import { useApi } from '../contexts/ApiProvider';
 
 export default function UserPage() {
+  const [user, setUser] = useState();
   const { username } = useParams();
+  const api = useApi();
+
+  useEffect(() => {
+    (async () => {
+      const response = await api.get('/users/' + username);
+      setUser(response.ok ? response.body : null);
+    })();
+  }, [username, api]);
 
   return (
     <Body sidebar>
-      <h1>{username}</h1>
-      <p>TODO</p>
+      {user === undefined ?
+        <Spinner animation="border" />
+      :
+        <>
+          {user === null ?
+            <p>User not found.</p>
+          :
+            <>
+              <Stack direction="horizontal" gap={4}>
+                <Image src={user.avatar_url + '&s=128'} roundedCircle />
+                <div>
+                  <h1>{user.username}</h1>
+                  {user.about_me && <h5>{user.about_me}</h5>}
+                  <p>
+                    Member since: <TimeAgo isoDate={user.first_seen} />
+                    <br />
+                    Last seen: <TimeAgo isoDate={user.last_seen} />
+                  </p>
+                </div>
+              </Stack>
+              <Posts content={user.id} />
+            </>
+          }
+        </>
+      }
     </Body>
   );
 }
