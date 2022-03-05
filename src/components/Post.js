215d78a18ce3a9e2f9cd0002dd98f346a77db8a0
Miguel Grinberg <miguel.grinberg@gmail.com>
2022-03-05 16:33:37 +0000

    Chapter 10: Memoization (v0.10)

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,10 @@
+import { memo } from 'react';
 import Stack from 'react-bootstrap/Stack';
 import Image from 'react-bootstrap/Image';
 import { Link } from 'react-router-dom';
 import TimeAgo from './TimeAgo';
 
-export default function Post({ post }) {
+export default memo(function Post({ post }) {
   return (
     <Stack direction="horizontal" gap={3} className="Post">
       <Image src={post.author.avatar_url + '&s=48'}
@@ -20,4 +21,4 @@ export default function Post({ post }) {
       </div>
     </Stack>
   );
-}
+});
