b7a6b067e95f5d646709d4c5bac3d1f9634a7a19
Miguel Grinberg <miguel.grinberg@gmail.com>
2022-03-09 11:00:40 +0000

    Chapter 11: Automated Testing (v0.11)

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders brand element', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const element = screen.getByText(/Microblog/);
+
+  expect(element).toBeInTheDocument();
+  expect(element).toHaveClass('navbar-brand');
 });
